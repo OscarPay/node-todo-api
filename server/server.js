7f6aad3bdbf0411d2004f56064a52d547362258a
oscar <oscar.pay4@gmail.com>
2018-02-15 16:18:00 -0600

    refactored server script and added POST /todos

--- server/server.js
+++ server/server.js
@@ -1,26 +1,26 @@
-var mongoose = require('mongoose');
+let express = require('express');
+let bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+let {mongoose} = require('./db/mongoose');
+let {Todo} = require('./models/todo');
+let {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
-    }
+let app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(err)
+    })
 });
 
-var newTodo = new Todo({
-    text: 'Cook dinner'
-});
-
-newTodo.save().then((doc)=> {
-    console.log('Saved todo', doc);
-}, (e) => {
-    console.log('Unable to save todo');
+app.listen(3000, () => {
+    console.log('Started on port 3000');
 });
