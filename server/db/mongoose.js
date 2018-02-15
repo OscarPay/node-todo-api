bef9673a05e29f112d1e905c0eabc55fe7b476e4
oscar <oscar.pay4@gmail.com>
2018-02-15 17:53:54 -0600

    added superuser

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 let db = {
     localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://<user>:<pass>@ds149268.mlab.com:49268/todoapp'
+    mlab: 'mongodb://admin:admin@ds149268.mlab.com:49268/todoapp'
 };
 
 mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
