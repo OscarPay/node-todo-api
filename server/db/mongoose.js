0810b8aef232b0ab2232c5250acdb8595b2e7d2c
oscar <oscar.pay4@gmail.com>
2018-02-16 06:04:51 -0600

    fixed the mlab uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 let db = {
     localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://admin:admin@ds149268.mlab.com:49268/todoapp'
+    mlab: 'mongodb://admin:admin@ds059284.mlab.com:59284/todoapp'
 };
 
 mongoose.connect(process.env.PORT ? db.mlab : db.localhost, {useMongoClient: true})
